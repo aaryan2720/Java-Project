8cacf849ffe1ce0332f3e95fffdf91b5d84450bf
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-12-13 12:04:21 +0530

    "Removed two blank lines from StockArrayList.java"

--- StockArrayList.java
+++ StockArrayList.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
-
-
 //will be used to hold the available stock to be bought.
 public class StockArrayList
 {
